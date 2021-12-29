79efe0af6253b3b39d541e3d815530c03cf5d7a3
MaxMhonc <mhonc@softserveinc.com>
2021-12-29 12:54:33 +0200

    [Added] flyweght and front controller pattern

---
+++ structural/flyweight/from_linuxtut/main.py
@@ -0,0 +1,12 @@
+import sys
+from flyweight.big_char_factory import BigCharFactory, BigString
+
+
+def startMain(string):
+    bs = BigString(string)
+    bs.print()
+
+
+if __name__ == '__main__':
+    # startMain(sys.argv[1])
+    startMain([0,1,2,3])
